1cee45a4d1fba77594bb993f87b23720e9db591f
tiarebalbi <tiarebalbi@me.com>
2012-08-25 18:03:43 -0300

    Atualizacao

---
+++ app/controllers/chats.js
@@ -0,0 +1,11 @@
+var Chats = function () {
+  this.respondsWith = ['html', 'json', 'xml', 'js', 'txt'];
+
+  this.index = function (req, resp, params) {
+    this.respond({params: params});
+  };
+
+};
+
+exports.Chats = Chats;
+
