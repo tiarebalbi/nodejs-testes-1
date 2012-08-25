79bb12eb964d3680833e264382ce2479d6d2013e
tiarebalbi <tiarebalbi@me.com>
2012-08-25 20:39:31 -0300

    Update

--- app/controllers/chats.js
+++ app/controllers/chats.js
@@ -2,7 +2,7 @@ var Chats = function () {
   this.respondsWith = ['html', 'json', 'xml', 'js', 'txt'];
 
   this.index = function (req, resp, params) {
-    this.respond({params: params});
+    this.respond({params: params}); 
   };
 
 };
